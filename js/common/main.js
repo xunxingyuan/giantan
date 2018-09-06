89e4d07b71080ae07183df754a089a19e66e2b6a
xunxingyuan <xunxingyuan@gmail.com>
2018-09-06 15:32:10 +0800

    add other

--- js/common/main.js
+++ js/common/main.js
@@ -1,5 +1,14 @@
+//加载公共模块
 $(document).ready(function () {
-    console.log("ready!");
-    $(".headerBox").load('./../../common/header.html')
-    $(".footerBox").load('./../../common/footer.html')
+    $(".headerBox").load('./../../common/header.html');
+    $(".footerBox").load('./../../common/footer.html');
+});
+
+//修改header背景
+$(document).scroll(function(e){
+    if($(this).scrollTop()=== 0){
+        $('header').css('background','transparent');
+    }else{
+        $('header').css('background','#142132');
+    }
 });
