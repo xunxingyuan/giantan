7dce7058a51f87c968af6833e3801e25e69a5ab2
xunxingyuan <xunxingyuan@gmail.com>
2019-01-05 13:43:26 +0800

    add change

--- js/common/main.js
+++ js/common/main.js
@@ -2,15 +2,24 @@
 $(document).ready(function () {
     $(".headerBox").load('./../../common/header.html');
     $(".footerBox").load('./../../common/footer.html');
+
+    $('.ctrlItem').bind('click', function (e) {
+        console.log(e)
+        $('.ctrlItem').removeClass('active')
+        $(this).addClass('active')
+    });
 });
 
 //修改header背景
-$(document).scroll(function(e){
-    if($(this).scrollTop()=== 0){
-        $('.headerBox').css('background','transparent');
-        $('.navBox a').css('color','#fff');
-    }else{
-        $('.headerBox').css('background','#fff');
-        $('.navBox a').css('color','#333');
+$(document).scroll(function (e) {
+    if ($(this).scrollTop() === 0) {
+        $('.headerBox').css('background', 'transparent');
+        // $('.headerBox').css('border', 'none');
+        // $('.navBox .link').css('color', '#fff');
+    } else {
+        $('.headerBox').css('background', '#142132');
+        // $('.headerBox').css('border-bottom', 'solid 1px #ddd');
+        // $('.navBox .link').css('color', '#333');
     }
+
 });
